6dd58a70a0290236aa55e13183135b0035b4eec1
github tamumisa <lucky.misap@gmail.com>
2023-04-14 22:02:20 +0900

    Qust5Done

--- week_1-2/practice4.rb
+++ week_1-2/practice4.rb
@@ -1,16 +1,34 @@
 # 2つの整数 x, y の大小を比較し、xとyの大小関係を出力する関数 greater を定義してください。
 
 # x,yを引数に受け取るgreater関数を作成
-def greater(x,y)
-    if x > y
-        puts ("x > y")
-    elsif x < y
-        puts ("x < y")
-    elsif x == y
-        puts ("x == y")
+# def greater(x,y)
+#     if x > y
+#         puts ("x > y")
+#     elsif x < y
+#         puts ("x < y")
+#     elsif x == y
+#         puts ("x == y")
+#     end
+# end
+# greater(5 ,4 )
+# greater(-50 ,-40)
+# greater(10 ,10)
+
+# 電車の料金
+# 東京駅から新宿駅までJR山手線に乗って移動します。
+# 12歳以上は大人料金で200円、6歳以上12歳未満はこども料金で100円、6歳未満は幼児で無料となります。
+# 年齢に応じて東京駅から新宿駅までの料金を出力する関数 train_fare を定義してください。
+
+def train_fare(x)
+    if x >= 12
+        puts "200円です"
+    elsif x >= 6
+        puts "100円"
+    elsif x < 6
+        puts "無料です"
     end
 end
-greater(5 ,4 )
-greater(-50 ,-40)
-greater(10 ,10)
 
+train_fare(12)
+train_fare(6)
+train_fare(5)
