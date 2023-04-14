485a2aae83212a92961f839630c968f5a45cec2e
github tamumisa <lucky.misap@gmail.com>
2023-04-14 22:19:52 +0900

    ブーリアン型課題Done

--- week_1-2/practice4.rb
+++ week_1-2/practice4.rb
@@ -19,16 +19,32 @@
 # 12歳以上は大人料金で200円、6歳以上12歳未満はこども料金で100円、6歳未満は幼児で無料となります。
 # 年齢に応じて東京駅から新宿駅までの料金を出力する関数 train_fare を定義してください。
 
-def train_fare(x)
-    if x >= 12
-        puts "200円です"
-    elsif x >= 6
-        puts "100円"
-    elsif x < 6
-        puts "無料です"
+# def train_fare(x)
+#     if x >= 12
+#         puts "200円です"
+#     elsif x >= 6
+#         puts "100円"
+#     elsif x < 6
+#         puts "無料です"
+#     end
+# end
+
+# train_fare(12)
+# train_fare(6)
+# train_fare(5)
+
+# 2つのブーリアン値 x と y が与えられ、x と y のうちどちらかが真なら true を出力し、
+# 両方真もしくは両方偽なら false を出力する関数 xor を定義してください。
+
+def xor(x,y)
+    if x == y
+        puts false
+    elsif x != y
+        puts true
     end
 end
 
-train_fare(12)
-train_fare(6)
-train_fare(5)
+xor(true, true)
+xor(true, false)
+xor(false, true)
+xor(false, false)
