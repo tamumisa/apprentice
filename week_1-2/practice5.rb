189ed6da5b224197171aa4a8e38b21ce007167bf
github tamumisa <lucky.misap@gmail.com>
2023-04-15 10:02:21 +0900

    LOOPDONE

--- week_1-2/practice5.rb
+++ week_1-2/practice5.rb
@@ -10,25 +10,33 @@
 
 # 羊が1匹 羊が2匹 羊が3匹 ...と、n 回羊の数えを出力する関数 sheep を定義してください。
 
-def sheep(n)
-    for num in 1..n do
-        puts "羊が#{num}匹"
-    end
-end
-sheep(3)
+# def sheep(n)
+#     for num in 1..n do
+#         puts "羊が#{num}匹"
+#     end
+# end
+# sheep(3)
 
 # 1 から 100 までの足し算の結果を出力する関数 sum_1_100 を定義してください。
 
-def sum_1_100
-    # 関数を完成させてください
-end
+# def sum_1_100()
+#     result = 0
+#     for i in 1..100
+#         result += i
+#     end
+#     puts result
+# end
 
-sum_1_100()
+# sum_1_100()
 
 # 2つの整数 x と y が与えられ、x から y までの足し算の結果を出力する関数 sum を定義してください。
 
 def sum(x, y)
-    # 関数を完成させてください
+    result = 0
+    for i in x..y
+        result += i
+    end
+    puts result
 end
 
 sum(10, 80)
