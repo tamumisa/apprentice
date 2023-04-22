7b5df352e5f09c524e66ce656456cf6d08a7ff42
github tamumisa <lucky.misap@gmail.com>
2023-04-22 19:31:23 +0900

    @player_scoreと@dealer_scoreの作成

--- blackjack/game.rb
+++ blackjack/game.rb
@@ -6,14 +6,24 @@ class Game
     def initialize
         @deck = Deck.new
         @player = Player.new('あなた')
+        @dealer = Player.new('ディーラー')
+        @player_score = 0
+        @dealer_score = 0
     end
 
     def play
         puts "ブラックジャックを開始します"
-        @player.draw(@deck,2)
-        binding.break
-        puts "あなたの引いたカードは#{@player.cards[0].to_s}です。"
-        puts "あなたの引いたカードは#{@player.cards[1].to_s}です。"
+        @player_score = @player.draw(@deck,2)
+        puts  @player_score
+
+        puts "#{@player.name}の引いたカードは#{@player.cards[0].to_s}です。"
+        puts "#{@player.name}の引いたカードは#{@player.cards[1].to_s}です。"
+
+        @dealer.draw(@deck,2)
+        puts "#{@dealer.name}の引いたカードは#{@dealer.cards[0].to_s}です"
+        puts "ディーラーの引いた2枚目のカードはわかりません"
+
+        puts "#{@player.name}の現在の得点は#{@player.cards[0].to_s}です。カードを引きますか？"
 
     end
 end
