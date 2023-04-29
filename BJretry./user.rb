1fb78f39367c9f93632c76ca74d59a62e9b2554a
github tamumisa <lucky.misap@gmail.com>
2023-04-30 00:27:15 +0900

    player_score計算完了

--- BJretry./user.rb
+++ BJretry./user.rb
@@ -1,41 +1,48 @@
 require_relative "deck"
 
 class User
-    attr_accessor :cards , :score, :role
+    attr_accessor :score
 
-    def initialize
+    def initialize(deck, role)
         @hand = []
         @score = 0
-        @role = "あなた"
+        @role = role
+        @cards = deck.cards
     end
 
-    def draw_card(deck)
-        puts deck.first
-        user_card = deck.take_out_card
-        @hand << user_card
-        deck.remove
+    def draw_card
+        @cards.shift
     end
     # 引いたカードを表示するメソッドを作成する
-    def present_a_card(deck)
-        puts "#{role}の引いたカードは#{deck.cards[0][:suit]}です"
+    def present_a_card(drawed_card)
+        puts "#{@role}の引いたカードは#{drawed_card[:suit]}です"
     end
 
-    def first_plyer_turn(deck)
+    def first_player_turn
         puts "あなたのターンです"
         puts "カードを2枚引きます"
         # ①draw_cardメソッドの処理を2回繰り返し、2枚のカードを引いた結果を表示する
         2.times do
-            draw_card(deck)
-            present_a_card(deck)
+            drawed_card = draw_card
+            present_a_card(drawed_card)
+            calc_score(drawed_card)
         end
     end
 
-    # ③スコアを計算するメソッドを作成する
-    def calc_score
-        @score = @hand
-        puts @score
+    def first_dealer_turn
+        puts "ディーラーのターンです"
+        present_a_card(draw_card)
+        draw_card
+        puts "ディーラーの2枚目のカードは分かりません"
     end
 
+    # ③スコアを計算するメソッドを作成する
+    def calc_score(drawed_card)
+        @score += drawed_card[:score]
+    end
 
-    # スコアを表示するメソッドを作成する
+    def current_score
+        puts "#{@role}の現在のスコアは#{@score}です"
+    end
+
 end
