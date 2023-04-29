17a9a20749b6e05017b566d86327e9e9921ad389
github tamumisa <lucky.misap@gmail.com>
2023-04-29 22:31:41 +0900

    プレイヤーが2回引くdraw_cardメソッド動いた

--- BJretry./user.rb
+++ BJretry./user.rb
@@ -11,28 +11,28 @@ class User
 
     def draw_card(deck)
         user_card = deck.take_out_card
-        puts user_card
-        hand << user_card
-        deck.remove(user_card)
+        @hand << user_card
+        # deck.remove(user_card)
     end
-
-    # def present_a_card
-    #     puts "#{role}の引いたカードは#{hand[0][:suit]}です"
-    # end
+    # 引いたカードを表示するメソッドを作成する
+    def present_a_card(deck)
+        puts "#{role}の引いたカードは#{deck.cards[0][:suit]}です"
+    end
 
     def first_plyer_turn(deck)
         puts "あなたのターンです"
         puts "カードを2枚引きます"
         # ①draw_cardメソッドの処理を2回繰り返し、2枚のカードを引いた結果を表示する
-        puts deck.cards[0][:suit]
+        2.times do
+            draw_card(deck)
+            present_a_card(deck)
+        end
     end
 
-    # 引いたカードを表示するメソッドを作成する
-    def present_a_card(deck)
-    puts "#{role}の引いたカードは#{deck.cards[0][:suit]}です"
-    end
+
 
     # ③スコアを計算するメソッドを作成する
+
 
     # スコアを表示するメソッドを作成する
 end
